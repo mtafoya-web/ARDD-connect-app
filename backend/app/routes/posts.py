f9e445f7f55a24169acd05699bd37169b0312664
Mario <tafoya1897@gmail.com>
2026-05-24 02:53:26 -0700

    -post issue

--- backend/app/routes/posts.py
+++ backend/app/routes/posts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session, joinedload
 from typing import List, Optional
 
